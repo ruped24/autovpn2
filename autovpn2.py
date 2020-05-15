2811d80ab92cc4d4ac8248eb64dab8dab3c830a9
Rupert Edwards <ruped24@gmail.com>
2020-05-15 12:46:36 -0400

    Update autovpn2.py

--- autovpn2.py
+++ autovpn2.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python2
 #
-# Autovpn2 Written by Rupe Wed May 13 2020, Version 1.0 
+# Autovpn2 Written by Rupe Wed May 13 2020, Version 1.0
 #
 # Connects to VPN Gate Public VPN Relay Servers.
-# 
+#
 # https://www.vpngate.net/en/
 #
 
@@ -25,6 +25,7 @@ class AutoVpn(object):
         self.openvpn()
 
     def save_config_file(self):
+        print "[autovpn2] writing config file"
         with open('/tmp/openvpnconf', 'w') as config_file:
             server = self.servers.index(self.country)
             config_file.write(
@@ -32,15 +33,12 @@ class AutoVpn(object):
                     str(b64decode(self.servers[server + 8])).split('\n')[:-1]
                 )
             )
+            print "[autovpn2] running openvpn\n"
 
     def get_serverlist(self):
         if not self.country:
             self.country = "US"
-        print "\n[%s] getting server list" % basename(__file__).split('.')[0]
-        print "[%s] looking for %s" % (
-            basename(__file__).split('.')[0], self.country
-        )
-        print "[%s] parsing response\n" % basename(__file__).split('.')[0]
+        print "[autovpn2] looking for %s" % self.country
 
         with closing(urlopen("http://www.vpngate.net/api/iphone/")
                     ) as serverlist:
@@ -48,12 +46,16 @@ class AutoVpn(object):
             self.servers = [x for x in serverlist if len(serverlist) > 15]
             try:
                 self.servers.index(self.country)
-                self.save_config_file()
             except ValueError:
                 exit(
                     "[\033[91m!\033[0m] Country code " + "\033[93m" +
                     self.country + "\033[0m" + " not in server list"
                 )
+            else:
+                try:
+                    self.save_config_file()
+                except:
+                    self.get_serverlist()
 
     def openvpn(self):
         call(['openvpn', '--config', '%s' % '/tmp/openvpnconf'])
@@ -62,8 +64,12 @@ class AutoVpn(object):
 if __name__ == '__main__':
     if geteuid() is not 0:
         exit("\033[91m[!]\033[0m Run as super user!")
+
     try:
+        print "\n[autovpn2] getting server list"
+        print "[autovpn2] parsing response"
         AutoVpn(''.join(argv[1:]))
+
     except KeyboardInterrupt:
         call(["kill", "-9", "%s" % getoutput('pidof openvpn')])
         if isfile("/tmp/openvpnconf"):
